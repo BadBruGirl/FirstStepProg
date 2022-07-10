dcbd9b0a02fad5062a9a55a3662673cea426547e
Ksu <kukinaksu@yandx.ru>
2022-07-10 20:22:49 +0300

    Add solution of task 4

--- SolutionTask4/Program.cs
+++ SolutionTask4/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    char[] M = inputLine.ToCharArray();
+    Console.WriteLine(M[2]);
+
+    // int inputNumber = int.Parse(inputLine);
+
+    // int outputNumber = inputNumber%10;
+    
+    // Console.WriteLine(outputNumber);
+}
+
+// string? inputLkneOne = Console.ReadLine();
+
+// if(inputLkneOne != null)
+// {
+//     string lastNum = inputLkneOne.Substring(2,1);
+//     Console.WriteLine(lastNum);
+// }
