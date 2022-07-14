dd67341a4dac457a411264aebd90617ead888297
Ksu <kukinaksu@yandx.ru>
2022-07-14 16:29:49 +0300

    Add solution of task 10

---
+++ HomeWorkSolutionTask10/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+    int findNumber = (inputNumber % 100)/10;
+
+    Console.WriteLine(findNumber);
+}    
