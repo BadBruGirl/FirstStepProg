baabcffca61a427a6282ba43eb0f358921d7989f
Ksu <kukinaksu@yandx.ru>
2022-07-14 12:21:30 +0300

    Added a task1 condition

--- SolutionTask1/Program.cs
+++ SolutionTask1/Program.cs
@@ -1,4 +1,6 @@
-﻿string? inputLkneOne = Console.ReadLine();
+﻿// Задача 1 Напишите программу, которая на вход принимает два числа и проверяет, являетс ли первое число квадратом второго.
+
+string? inputLkneOne = Console.ReadLine();
 string? inputLkneTwo = Console.ReadLine();
 
 if(inputLkneOne != null && inputLkneTwo != null)
