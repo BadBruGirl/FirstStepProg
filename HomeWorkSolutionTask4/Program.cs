60511997e0610c7a32c9a04297408925573c3c96
Ksu <kukinaksu@yandx.ru>
2022-07-14 12:38:38 +0300

    Added a Homeworktask4 condition

--- HomeWorkSolutionTask4/Program.cs
+++ HomeWorkSolutionTask4/Program.cs
@@ -1,4 +1,6 @@
-﻿string? inputLine = Console.ReadLine();
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+string? inputLine = Console.ReadLine();
 
 if(inputLine != null)
 {   
