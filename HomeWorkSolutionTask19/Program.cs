7795fec02e41bf0dd190dfb9403aa1b0fd13276c
Ksu <kukinaksu@yandx.ru>
2022-07-21 21:17:13 +0300

    Add new solution whith methods to homework task 19

--- HomeWorkSolutionTask19/Program.cs
+++ HomeWorkSolutionTask19/Program.cs
@@ -1,18 +1,47 @@
 ﻿//Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-string? inputLine = Console.ReadLine(); //открыли строку для ввода в консоль
+// string? inputLine = Console.ReadLine(); //открыли строку для ввода в консоль
 
-if(inputLine != null)  //проверили на null
+// if(inputLine != null)  //проверили на null
+// {
+//     int inputNumberP = int.Parse(inputLine); //превратили в вещественное число
+
+//     int digit1 = inputNumberP/10000; //нашли первую цифру числа
+//     int digit2 = (inputNumberP/1000)%10; //нашли вторую цифру числа
+//     int digit3 = (inputNumberP/100)%10; //нашли третью цифру числа
+//     int digit4 = (inputNumberP/10)%10; //нашли четвертую цифру числа
+//     int digit5 = inputNumberP%10; //нашли пятую цифру числа
+
+//     if (digit1 == digit5&&digit2==digit4) //сравнили 1и5 2и4 цифры числа
+//     {
+//         Console.WriteLine("да");
+//     }
+//     else
+//     {
+//         Console.WriteLine("нет");
+//     }
+// }
+
+int inputNumberP;
+int digit1;
+int digit2;
+int digit3;
+int digit4;
+int digit5;
+
+void findDigits(int inputNumberP)
 {
-    int inputNumberP = int.Parse(inputLine); //превратили в вещественное число
+    digit1 = inputNumberP / 10000; //нашли первую цифру числа
+    digit2 = (inputNumberP / 1000) % 10; //нашли вторую цифру числа
+    digit3 = (inputNumberP / 100) % 10; //нашли третью цифру числа
+    digit4 = (inputNumberP / 10) % 10; //нашли четвертую цифру числа
+    digit5 = inputNumberP % 10; //нашли пятую цифру числа
 
-    int digit1 = inputNumberP/10000; //нашли первую цифру числа
-    int digit2 = (inputNumberP/1000)%10; //нашли вторую цифру числа
-    int digit3 = (inputNumberP/100)%10; //нашли третью цифру числа
-    int digit4 = (inputNumberP/10)%10; //нашли четвертую цифру числа
-    int digit5 = inputNumberP%10; //нашли пятую цифру числа
+}
 
-    if (digit1 == digit5&&digit2==digit4) //сравнили 1и5 2и4 цифры числа
+void Palindrom(int digit1, int digit2, int digit4, int digit5)
+{
+    if (digit1 == digit5 && digit2 == digit4) //сравнили 1и5 2и4 цифры числа
     {
         Console.WriteLine("да");
     }
@@ -21,3 +50,9 @@ if(inputLine != null)  //проверили на null
         Console.WriteLine("нет");
     }
 }
+
+string? inputLine = Console.ReadLine(); //открыли строку для ввода в консоль
+inputNumberP = int.Parse(inputLine); //превратили в вещественное число
+
+findDigits(inputNumberP);
+Palindrom(digit1, digit2, digit4, digit5);
