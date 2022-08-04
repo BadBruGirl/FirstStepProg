ba571a93c5084502ea94cdac50a18325b504a914
Ksu <kukinaksu@yandx.ru>
2022-08-04 22:19:36 +0300

    Add solution of task 25 27 29

---
+++ HomeWorkSolutionTask25/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 25 Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int inputNumberA;
+int inputNumberB;
+
+
+void Read()
+{
+    Console.WriteLine("Введите первое число:");
+    string inputA = Console.ReadLine();
+    inputNumberA = int.Parse(inputA ?? "");
+
+    Console.WriteLine("Введите второе число:");
+    string inputB = Console.ReadLine();
+    inputNumberB = int.Parse(inputB ?? "");
+}
+
+
+int Conculate1(int inputNumberA, int inputNumberB)
+{
+    int result = 1;
+    result = (int)Math.Pow(inputNumberA, inputNumberB);
+    return result;
+}
+
+int Conculate2(int inputNumberA, int inputNumberB)
+{
+    int i = 0;
+    int result = 1;
+    while (i < inputNumberB)
+    {
+        result = result * inputNumberA;
+        i++;
+    }
+    return result;
+}
+void Print()
+{
+    Console.WriteLine(Conculate1(inputNumberA, inputNumberB));
+    Console.WriteLine(Conculate2(inputNumberA, inputNumberB));
+}
+
+Read();
+Conculate1(inputNumberA, inputNumberB);
+Conculate2(inputNumberA, inputNumberB);
+Print();
---
+++ HomeWorkSolutionTask27/Program.cs
@@ -0,0 +1,19 @@
+﻿// №27 Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число:");
+string? inputLine = Console.ReadLine();
+int inputNum = int.Parse(inputLine);
+int sum=0;
+
+void Conculate(int inputNum)
+{
+while (inputNum>0)
+{
+    sum=sum+inputNum%10;
+    inputNum=inputNum/10;
+}
+}
+
+Conculate(inputNum);
+
+Console.WriteLine(sum);
