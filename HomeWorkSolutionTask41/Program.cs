13d83a70fec2db96a812a0f147eacbde341af369
Ksu <kukinaksu@yandx.ru>
2022-08-11 20:39:20 +0300

    Add solutions of task 41 43

---
+++ HomeWorkSolutionTask41/Program.cs
@@ -0,0 +1,43 @@
+﻿// №41 Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int m;
+int count=0;
+
+int ReadNumbers()
+{
+    Console.Write($"Укажи планируемое количество чисел: ");
+    m = int.Parse(Console.ReadLine() ?? "");
+    return m;
+}
+
+int[] InputNumbers(int m)
+{
+    int[] massiveNumbers = new int[m];
+    for (int i = 0; i < m; i++)
+    {
+        Console.Write($"Введи {i + 1} число: ");
+        massiveNumbers[i] = int.Parse(Console.ReadLine()?? "");
+    }
+    return massiveNumbers;
+}
+
+
+int Comparison(int[] massiveNumbers)
+{
+    count = 0;
+    for (int i = 0; i < massiveNumbers.Length; i++)
+    {
+        if (massiveNumbers[i] > 0) count += 1;
+    }
+    return count;
+}
+
+
+void PrintAnswer()
+{
+    Console.WriteLine($"Введено чисел больше 0: {(count)} ");
+}
+
+ReadNumbers();
+int[] testArray=InputNumbers(m);
+Comparison(testArray);
+PrintAnswer();
---
+++ HomeWorkSolutionTask43/Program.cs
@@ -0,0 +1,50 @@
+﻿// №43 Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем
+
+int valueB1;
+int valueK1;
+int valueB2;
+int valueK2;
+int coordyX=0;
+int coordyY=0;
+string coordy;
+
+ReadingFunctionValues();
+Conculate();
+PrintPointOfIntersectionOfCoordinates();
+
+
+void ReadingFunctionValues()
+{
+    Console.WriteLine("Введите значение b1");
+    valueB1 = int.Parse(Console.ReadLine()?? "");
+
+    Console.WriteLine("Введите значение k1");
+    valueK1 = int.Parse(Console.ReadLine()?? "");
+
+    Console.WriteLine("Введите значение b2");
+    valueB2 = int.Parse(Console.ReadLine()?? "");
+
+    Console.WriteLine("Введите значение k2");
+    valueK2 = int.Parse(Console.ReadLine()?? "");
+}
+
+void Conculate()
+{
+    if ((valueK1 == valueK2)&&(valueB1 == valueB2))
+    
+        Console.WriteLine("Прямые совпадают");
+    else
+        if (valueK1 == valueK2)
+        Console.WriteLine("Прямые совпадают");        
+    else
+        {
+            coordyX=(valueB2-valueB1)/(valueK1-valueK2);
+            coordyY=(valueK1*(valueB2-valueB1))/(valueK1-valueK2)+valueB1;
+        }  
+    coordy = "Координаты точки пересечения: " + coordyX + "," + coordyY;
+}
+
+void PrintPointOfIntersectionOfCoordinates()
+{
+    Console.WriteLine(coordy);
+}    
